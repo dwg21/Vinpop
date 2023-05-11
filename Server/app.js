a0448e313a2951f9710ec47c6706a6f852592151
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-05-11 11:26:08 -0500

    rever

---
+++ Server/app.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+require('express-async-errors');
+// express
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+// rest of the packages
+
+
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+
+// database
+const connectDB = require('./db/connect');
+
+//  routers
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const listingRouter = require('./routes/listingRoutes');
+const cartRouter = require('./routes/CartRoutes');
+const offerRoute  = require('./routes/offerRoutes');
+
+
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// app.set('trust proxy', 1);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000,
+//     max: 60,
+//   })
+// );
+// app.use(helmet());
+app.use(cors({
+    origin:'http://localhost:3000',
+    credentials: true
+}));
+// app.use(xss());
+// app.use(mongoSanitize());
+
+// app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', express: true}));
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(fileUpload({useTempFiles: true}));
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/listing', listingRouter);
+app.use('/api/v1/cart', cartRouter);
+app.use('/api/v1/offer', offerRoute);
+
+
+
+
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ Server/controllers/userController.js
@@ -0,0 +1,133 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const {
+  createTokenUser,
+  attachCookiesToResponse,
+  checkPermissions,
+} = require('../utils');
+
+const getAllUsers = async (req, res) => {
+  console.log(req.user);
+  const users = await User.find({ role: 'user' }).select('-password');
+  res.status(StatusCodes.OK).json({ users });
+};
+
+const getSingleUser = async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
+};
+
+const showCurrentUser = async (req, res) => {
+  res.status(StatusCodes.OK).json({ user: req.user });
+};
+// update user with user.save()
+const updateUser = async (req, res) => {
+  const { email, name } = req.body;
+  if (!email || !name) {
+    throw new CustomError.BadRequestError('Please provide all values');
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.email = email;
+  user.name = name;
+
+  await user.save();
+
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+const updateUserPassword = async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError('Please provide both values');
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+  user.password = newPassword;
+
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated.' });
+};
+
+
+const viewFavorites = async (req, res) => {
+  const user = await User.findOne({ _id: req.user.userId });
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+  const favorites = user.favorites;
+  res.status(StatusCodes.OK).json({favorites});
+
+
+}
+
+const addFavorite = async (req, res) => {
+  const {listingId} = req.body;
+  console.log(listingId);
+
+  const user = await User.findOne({ _id: req.user.userId });
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+
+  user.favorites = [...user.favorites, listingId];
+
+  await user.save();
+  
+  res.status(StatusCodes.OK).json({user});
+
+
+
+}
+
+const removeFavorite = async (req, res) => {
+  const {listingId} = req.query;
+  console.log(req.listingId);
+  const user = await User.findOne({ _id: req.user.userId });
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+
+  user.favorites = user.favorites.filter(listing => listing !== listingId);
+  await user.save()
+
+  res.status(StatusCodes.OK).json({user})
+  
+
+}
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+  addFavorite,
+  removeFavorite,
+  viewFavorites
+};
+
+// update user with findOneAndUpdate
+// const updateUser = async (req, res) => {
+//   const { email, name } = req.body;
+//   if (!email || !name) {
+//     throw new CustomError.BadRequestError('Please provide all values');
+//   }
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.user.userId },
+//     { email, name },
+//     { new: true, runValidators: true }
+//   );
+//   const tokenUser = createTokenUser(user);
+//   attachCookiesToResponse({ res, user: tokenUser });
+//   res.status(StatusCodes.OK).json({ user: tokenUser });
+// };
